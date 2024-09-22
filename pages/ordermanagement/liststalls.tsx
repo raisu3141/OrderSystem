c58bb3d2b32d2c3bd483dbf78296e1efe3935bca
Yuta Matayoshi <yuuta.matayoshi1413@gmail.com>
2024-09-22 16:37:24 +0900

    update 未使用のインポートを削除

--- pages/ordermanagement/liststalls.tsx
+++ pages/ordermanagement/liststalls.tsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
 import StallsCard from '../../components/StallsCard'
-import Image from 'next/image'
 
 
 interface Stall {
