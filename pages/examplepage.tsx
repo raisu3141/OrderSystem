643ca702b5869e1b5726fefbb761c83ebb9fb7f1
Yuta Matayoshi <yuuta.matayoshi1413@gmail.com>
2024-09-22 15:03:04 +0900

    add 画面遷移の例を作成

---
+++ pages/examplepage.tsx
@@ -0,0 +1,36 @@
+import exp from "constants";
+import localFont from "next/font/local";
+import Link from "next/link";
+
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export  function ExamplePage() {
+
+    return (
+        <div
+          className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+        >
+          <h1 className="text-4xl font-bold">
+            画面遷移の例だよ！
+          </h1>
+          <Link 
+            href="/" 
+            className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors"
+          >
+            Rootのページに戻る
+          </Link>
+        </div>
+    )
+}
+
+export default ExamplePage;
