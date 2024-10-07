5f22ef440dd63ebafaface04ff232465a3d1cb11
Yuta Matayoshi <yuuta.matayoshi1413@gmail.com>
2024-10-07 10:53:57 +0900

    update stallpageに引数を持たせてコンポーネント化っぽくした

--- pages/ordermanagement/stallpage.tsx
+++ pages/ordermanagement/stallpage.tsx
@@ -1,30 +1,34 @@
-import Head from "next/head";
+import { useSearchParams } from 'next/navigation'
+import { Metadata } from 'next'
 import Styles from "../../styles/Home.module.css";
 import OrderticketManager from "../../components/ordermanege/orderticketmanager";
 
 
-interface StallPageProps {
-  storeName: string;
+export const metadata: Metadata = {
+  title: 'Stall Page',
 }
 
-export default function StallPage1({ storeName }: StallPageProps) {
+export default function StallPage() {
+  const searchParams = useSearchParams()
+  const storeName = searchParams.get('storeName')
+  const name = searchParams.get('name')
 
-  //あとで変える
-  storeName = "demostore";
-
+  if (!storeName || !name) {
     return (
       <div>
-          <Head>
-              <title>
-                屋台１
-              </title>
-          </Head>
-          <div className={Styles.container}>
-            <div className="sticky top-0 bg-white border-b-2 border-gray-300 p-4 z-10 flex items-center justify-between mb-8">
-              <h1 className="text-4xl font-bold">屋台１</h1>
-            </div>
-            <OrderticketManager storeName={storeName} />
-          </div>
+        Error: Missing parameters
       </div>
     )
+  }
+
+  return (
+    <div>
+      <div className={Styles.container}>
+        <div className="sticky top-0 bg-white border-b-2 border-gray-300 p-4 z-10 flex items-center justify-between mb-8">
+          <h1 className="text-4xl font-bold">{name}</h1>
+        </div>
+        <OrderticketManager storeName={storeName} />
+      </div>
+    </div>
+  )
 }
