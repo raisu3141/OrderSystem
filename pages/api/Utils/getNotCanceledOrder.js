8f751b195d2f2e4f7f596289bdc4a16e22993ad0
SeinKina <109860726+SeinKina@users.noreply.github.com>
2024-10-20 11:03:44 +0900

    [Update] getNotCanceledOrder.jsの更新

--- pages/api/Utils/getNotCanceledOrder.js
+++ pages/api/Utils/getNotCanceledOrder.js
@@ -1,2 +1,22 @@
 // pages/api/Utils/getNotCanceledOrder.js
 import connectToDatabase from "../../../lib/mongoose";
+import OrderData from "../../../models/OrderData";
+import "../../../models/ProductData";
+
+export default async function handler(req, res) {
+  await connectToDatabase();
+
+  try {
+    const cancellableOrders = await OrderData.find(
+      { cancelStatus: false, cancellableStatus: false },
+      "ticketNumber clientName orderList.orderQuantity",
+    ).populate('orderList.productId', 'productName price');
+
+    console.log(cancellableOrders);
+
+    res.status(200).json(cancellableOrders);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'エラーが発生しました', error: error.message });
+  }
+}
