92ac55523965ae97d2ed3bab3da32fbf299a7a6a
SeinKina <109860726+SeinKina@users.noreply.github.com>
2024-10-21 13:42:33 +0900

    [Update] storeWaitTimeSuber.jsの更新
    引数がorderListのstoreWaitTimeSuber2メソッドの追加

--- pages/api/Utils/storeWaitTimeSuber.js
+++ pages/api/Utils/storeWaitTimeSuber.js
@@ -49,3 +49,44 @@ export const storeWaitTimeSuber = async (req, res) => {
         return res.status(500).json({ message: 'エラーが発生しました' });
     }
 }
+
+export const storeWaitTimeSuber2 = async (orderList) => {
+    await connectToDatabase();
+    try {
+        let subtime = 0; // 初期化
+
+        console.log(orderList);
+        for (const order of orderList) {
+            // 注文リストの取得
+            const { productId, storeName, orderQuantity } = order;
+
+            // メニューと屋台を取得
+            const product = await ProductData.findById(productId);
+            const store = await StoreData.findOne({ storeName: storeName });
+
+            // メニューと屋台が取れたかエラー処理
+            if (!product || !store) {
+                // return res.status(404).json({ message: '商品または屋台が見つかりませんでした。' });
+                return { message: '商品または屋台が見つかりませんでした。' };
+            }
+
+            const cookTime = product.cookTime * orderQuantity;
+            subtime += cookTime;
+        }
+
+        // 屋台の待ち時間更新
+        const store = await StoreData.findOne({ storeName: orderList[0].storeName }); // 最後の注文のstoreNameで取得
+        store.storeWaitTime -= subtime;
+
+        await store.save();
+
+        // データを返す必要はないが、処理が成功したことを返す
+        // return res.status(200).json({ status: true });
+        return { status: true };
+
+    } catch (error) {
+        console.error(error);
+        // return res.status(500).json({ message: 'エラーが発生しました' });
+        return { message: 'エラーが発生しました' };
+    }
+}
